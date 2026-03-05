d7685011b5817fc0970b711b7c83f7f92300b9bf
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-03-05 15:48:48 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+export const content = ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"];
+export const theme = {
+    extend: {},
+};
+export const plugins = [require("daisyui")];
